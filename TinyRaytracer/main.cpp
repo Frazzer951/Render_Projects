de3f86bd6f5fb4a0e1b6dc2eca86f7167e319fad
Luke Eltiste <luke343279@gmail.com>
2021-08-17 16:42:28 -0700

    run through clang tidy

--- TinyRaytracer/main.cpp
+++ TinyRaytracer/main.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
 
 #include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <limits>
 #include <vector>
@@ -38,15 +39,14 @@ bool ray_sphere_intersect( const vec3 & orig, const vec3 & dir, const Sphere & s
   t0        = tca - thc;
   float t1  = tca + thc;
   if( t0 < 1e-3 ) t0 = t1;    // offset the original point to avoid occlusion by the object itself
-  if( t0 < 1e-3 ) return false;
-  return true;
+  return t0 >= 1e-3;
 }
 
-vec3 reflect( const vec3 & I, const vec3 & N ) { return I - N * 2.f * ( I * N ); }
+vec3 reflect( const vec3 & I, const vec3 & N ) { return I - N * 2.F * ( I * N ); }
 
-vec3 refract( const vec3 & I, const vec3 & N, const float eta_t, const float eta_i = 1.f )
+vec3 refract( const vec3 & I, const vec3 & N, const float eta_t, const float eta_i = 1.F )
 {    // Snell's law
-  float cosi = -std::max( -1.f, std::min( 1.f, I * N ) );
+  float cosi = -std::max( -1.F, std::min( 1.F, I * N ) );
   if( cosi < 0 )
     return refract( I, -N, eta_i, eta_t );    // if the ray comes from the inside the object, swap the air and the media
   float eta = eta_i / eta_t;
@@ -62,7 +62,7 @@ bool scene_intersect( const vec3 & orig, const vec3 & dir, const std::vector<Sph
   float spheres_dist = std::numeric_limits<float>::max();
   for( const Sphere & s : spheres )
   {
-    float dist_i;
+    float dist_i = NAN;
     if( ray_sphere_intersect( orig, dir, s, dist_i ) && dist_i < spheres_dist )
     {
       spheres_dist = dist_i;
@@ -77,13 +77,13 @@ bool scene_intersect( const vec3 & orig, const vec3 & dir, const std::vector<Sph
   {                                        // avoid division by zero
     float d  = -( orig.y + 4 ) / dir.y;    // the checkerboard plane has equation y = -4
     vec3  pt = orig + dir * d;
-    if( d > 1e-3 && fabs( pt.x ) < 10 && pt.z < -10 && pt.z > -30 && d < spheres_dist )
+    if( d > 1e-3 && std::fabs( pt.x ) < 10 && pt.z < -10 && pt.z > -30 && d < spheres_dist )
     {
       checkerboard_dist = d;
       hit               = pt;
       N                 = vec3 { 0, 1, 0 };
       material.diffuse_color =
-          ( int( .5 * hit.x + 1000 ) + int( .5 * hit.z ) ) & 1 ? vec3 { .3, .3, .3 } : vec3 { .3, .2, .1 };
+          ( ( int( .5 * hit.x + 1000 ) + int( .5 * hit.z ) ) & 1 ) != 0 ? vec3 { .3, .3, .3 } : vec3 { .3, .2, .1 };
     }
   }
   return std::min( spheres_dist, checkerboard_dist ) < 1000;
@@ -92,7 +92,8 @@ bool scene_intersect( const vec3 & orig, const vec3 & dir, const std::vector<Sph
 vec3 cast_ray( const vec3 & orig, const vec3 & dir, const std::vector<Sphere> & spheres,
                const std::vector<Light> & lights, size_t depth = 0 )
 {
-  vec3     point, N;
+  vec3     point;
+  vec3     N;
   Material material;
 
   if( depth > 4 || !scene_intersect( orig, dir, spheres, point, N, material ) )
@@ -103,21 +104,23 @@ vec3 cast_ray( const vec3 & orig, const vec3 & dir, const std::vector<Sphere> & 
   vec3 reflect_color = cast_ray( point, reflect_dir, spheres, lights, depth + 1 );
   vec3 refract_color = cast_ray( point, refract_dir, spheres, lights, depth + 1 );
 
-  float diffuse_light_intensity = 0, specular_light_intensity = 0;
+  float diffuse_light_intensity  = 0;
+  float specular_light_intensity = 0;
   for( const Light light : lights )
   {
     vec3 light_dir = ( light.position - point ).normalize();
 
-    vec3     shadow_pt, trashnrm;
+    vec3     shadow_pt;
+    vec3     trashnrm;
     Material trashmat;
     if( scene_intersect( point, light_dir, spheres, shadow_pt, trashnrm, trashmat )
         && ( shadow_pt - point ).norm()
                < ( light.position - point ).norm() )    // checking if the point lies in the shadow of the light
       continue;
 
-    diffuse_light_intensity += light.intensity * std::max( 0.f, light_dir * N );
+    diffuse_light_intensity += light.intensity * std::max( 0.F, light_dir * N );
     specular_light_intensity +=
-        std::pow( std::max( 0.f, -reflect( -light_dir, N ) * dir ), material.specular_exponent ) * light.intensity;
+        std::pow( std::max( 0.F, -reflect( -light_dir, N ) * dir ), material.specular_exponent ) * light.intensity;
   }
   return material.diffuse_color * diffuse_light_intensity * material.albedo[0]
        + vec3 { 1., 1., 1. } * specular_light_intensity * material.albedo[1] + reflect_color * material.albedo[2]
